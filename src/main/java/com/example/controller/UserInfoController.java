dd55ce2b701e62584c592a7dd80778c6b7566493
marui <markmarui@vip.qq.com>
2019-09-04 23:17:02 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Marie
+ * @date 2019/9/4 10:13 PM
+ *
+ **/
+@RestController
+public class UserInfoController {
+
+}
---
+++ src/main/java/com/example/model/UserInfoDao.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.entity.UserInfoEntity;
+
+/**
+ * @author Marie
+ * @date 2019/9/4 10:09 PM
+ **/
+public interface UserInfoDao extends BaseMapper<UserInfoEntity> {
+
+}
